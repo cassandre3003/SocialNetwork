a683298444eefacc8313b5a2c37c207916aedf93
suzan <suzannejb@outlook.fr>
2019-05-21 19:22:36 +0200

    mise a jour de l'interface, possiblilté d'afficher les noms des utilisateurs

--- src/main/java/fr/ul/miage/SocialNetwork/interfaceApp/ControllerAccueil.java
+++ src/main/java/fr/ul/miage/SocialNetwork/interfaceApp/ControllerAccueil.java
@@ -3,29 +3,45 @@ package fr.ul.miage.SocialNetwork.interfaceApp;
 
 import fr.ul.miage.SocialNetwork.file.Reader;
 import fr.ul.miage.SocialNetwork.graph.Graph;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ControllerAccueil {
+public class ControllerAccueil  implements Initializable {
     @FXML
-    private ListView<String> lien;
+    private ChoiceBox lien;
     @FXML
-    private ListView<String> listeDeDépart;
+    private ChoiceBox listeDeDepart;
     @FXML
     private ListView parcours;
 
-    void initialize(URL url, ResourceBundle rb) throws IOException {
-        Reader reader = new Reader();
-        Graph graphe  =  reader.creerGraph();
-        //lien.setItems(graphe.getLiens());
-        System.out.println(lien.toString());
-        listeDeDépart.getItems().add("Cassandre");
-        listeDeDépart.getItems().add("Salem");
-        listeDeDépart.getItems().add("Ouaoua");
+    @FXML
+    public void initialize(URL url, ResourceBundle rb) {
+        Reader reader = null;
+        try {
+            reader = new Reader();
+            Graph graphe  =  reader.creerGraph();
+
+            for (String str : graphe.getNomNoeuds()) {
+                listeDeDepart.getItems().add(str);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void adapteLien (ActionEvent e) {
+
 
     }
+
 }
