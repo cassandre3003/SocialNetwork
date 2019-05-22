ec0ee9c2e8161b5dd08a8e63da58eee10c5e77b6
suzan <suzannejb@outlook.fr>
2019-05-22 11:25:19 +0200

    mise a jour de l'interface, affichage de l'unicité, et des modes de parcours

--- src/main/java/fr/ul/miage/SocialNetwork/interfaceApp/ControllerAccueil.java
+++ src/main/java/fr/ul/miage/SocialNetwork/interfaceApp/ControllerAccueil.java
@@ -1,8 +1,8 @@
 package fr.ul.miage.SocialNetwork.interfaceApp;
 
-
 import fr.ul.miage.SocialNetwork.file.Reader;
 import fr.ul.miage.SocialNetwork.graph.Graph;
+import fr.ul.miage.SocialNetwork.graph.Lien;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,26 +12,35 @@ import javafx.scene.control.ListView;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashSet;
 import java.util.ResourceBundle;
 
-public class ControllerAccueil  implements Initializable {
+public class ControllerAccueil implements Initializable {
     @FXML
     private ChoiceBox lien;
     @FXML
     private ChoiceBox listeDeDepart;
     @FXML
-    private ListView parcours;
+    private ChoiceBox parcours;
+    @FXML
+    private ChoiceBox level;
+    @FXML
+    private ChoiceBox unicite;
 
     @FXML
     public void initialize(URL url, ResourceBundle rb) {
-        Reader reader = null;
         try {
-            reader = new Reader();
+            parcours.getItems().addAll("En longueur d'abord","En largeur d'abord");
+            unicite.getItems().addAll("NoeudGlobal", "relationGlobal");
+
+
+            Reader reader = new Reader();
             Graph graphe  =  reader.creerGraph();
 
             for (String str : graphe.getNomNoeuds()) {
                 listeDeDepart.getItems().add(str);
             }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -39,7 +48,13 @@ public class ControllerAccueil  implements Initializable {
         }
     }
 
-    public void adapteLien (ActionEvent e) {
+    public void adapteLien (ActionEvent e) throws IOException{
+        Reader reader = new Reader();
+        Graph graphe  =  reader.creerGraph();
+        System.out.println(listeDeDepart.getSelectionModel().getSelectedItem().toString());
+        for (String ch : graphe.typeLienParNomNoeud("Cassandra")) {
+            lien.getItems().add(ch);
+        }
 
 
     }
