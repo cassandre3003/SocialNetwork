2eaa29eb49c9d99066880eefe31c927ce86ca543
unknown <salme.khalifa@gmail.com>
2019-05-22 13:27:28 +0200

    Commit avant rebase

--- src/main/java/fr/ul/miage/SocialNetwork/Main.java
+++ src/main/java/fr/ul/miage/SocialNetwork/Main.java
@@ -1,4 +1,25 @@
 package fr.ul.miage.SocialNetwork;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        StackPane pane = new StackPane();
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+       // pane.getChildren().add(ControlerGraphe.getFXGraph(new Graph(new HashSet<>())));
+        primaryStage.show();
+    }
 }
---
+++ src/main/java/fr/ul/miage/SocialNetwork/interfacegraphe/ControlerGraphe.java
@@ -0,0 +1,64 @@
+package fr.ul.miage.SocialNetwork.interfacegraphe;
+
+import fr.ul.miage.SocialNetwork.file.Reader;
+import fr.ul.miage.SocialNetwork.graph.Graph;
+import fr.ul.miage.SocialNetwork.graph.Lien;
+import fr.ul.miage.SocialNetwork.graph.Noeud;
+import javafx.embed.swing.SwingNode;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+//import org.graphstream.graph.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.view.Viewer;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.concurrent.ExecutorService;
+
+public class ControlerGraphe
+{
+    @FXML
+    public AnchorPane anchor ;
+    @FXML
+    public Pane pane;
+
+    public static Node getFXGraph(Graph myGraph) throws IOException {
+        SwingNode fxGraph = new SwingNode();
+        org.graphstream.graph.Graph graph = new SingleGraph("test");
+
+        // construire le graphe a partir de myGraph
+        Reader reader = new Reader(); //pour lire le fichier
+        Graph graphFichier = reader.creerGraph(); //pour créer le graphe a partir de la methode creerGraph
+        HashSet<Noeud> noeuds = graphFichier.getNoeuds(); //pour récupérer les noeuds
+        HashSet<Lien> liens = graphFichier.getLiens(); //pour récupérer les liens
+
+
+        Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_GUI_THREAD);
+        fxGraph.setContent(viewer.addDefaultView(false));
+        return fxGraph;
+    }
+
+    public ExecutorService getExecutorService() {
+        return this.getExecutorService();
+    }
+
+    public  Node convertGraph(Graph cGraph){
+        SwingNode fxGraph = new SwingNode();
+        org.graphstream.graph.Graph graph = new SingleGraph("test");
+
+        Noeud claude = cGraph.getNoeudByNom("Claude");
+        Noeud pierre = cGraph.getNoeudByNom("Pierre");
+        Lien lien1 = cGraph.getLiens();
+
+
+        //convertir graphe en streamGraphe : convertir nos noeuds en noeuds de la bibiolthèque streamgraphe
+        Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_GUI_THREAD);
+        fxGraph.setContent(viewer.addDefaultView(false));
+        return fxGraph;
+
+    }
+
+
+}
